99387c15ed2a387181abfcc974d850276edcd464
Baiel <i.445@mail.ru>
2022-04-17 23:03:29 +0300

    add .env

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
 const TelegramApi = require('node-telegram-bot-api')
 const { options } = require('nodemon/lib/config')
-
-const token = '5312733665:AAEaSl3_1Gt64sWWohkNdTJwNFxKqG72ii4'
-
+const token = process.env.TOKEN
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on('message', msg => {
-    console.log(msg.text)
+bot.on('message', async msg => {
+    const text = msg.text;
+    const chatid = msg.chat.id;
+
+    if (text === '/booking') {
+        await bot.sendMessage(chatid, 'Бронирование')
+    }
+    if (text === '/place') {
+        await bot.sendMessage(chatid, 'Выберите центр')
+    }
 })
