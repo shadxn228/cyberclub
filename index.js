3b52473883920b491d29a59725e2706e20620e67
Baiel <i.445@mail.ru>
2022-04-21 20:05:44 +0300

    add button

--- index.js
+++ index.js
@@ -4,14 +4,56 @@ const { options } = require('nodemon/lib/config')
 const token = process.env.TOKEN
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on('message', async msg => {
-    const text = msg.text;
-    const chatid = msg.chat.id;
+const bookingOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text:'Главная', callback_data:'1'}], [{text:'Бронирование', callback_data:'2'}], [{text:'Меню', callback_data:'3'}]
+        ]
+    })
+}
 
-    if (text === '/booking') {
-        await bot.sendMessage(chatid, 'Бронирование')
-    }
-    if (text === '/place') {
-        await bot.sendMessage(chatid, 'Выберите центр')
-    }
-})
+const placeOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text:'Семеновская', callback_data:'4'}], [{text:'Выхино', callback_data:'5'}], 
+            [{text:'Южная', callback_data:'6'}], [{text:'Щелковская', callback_data:'7'}]
+            [{text:'Сокол', callback_data:'8'}], [{text:'Люблино', callback_data:'9'}]
+            [{text:'Коломенская', callback_data:'10'}], [{text:'Солнцево', callback_data:'11'}]
+        ]
+    })
+}
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начать'}
+    ])
+    
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatid = msg.chat.id;
+    
+        if (text === '/start') {
+            return bot.sendMessage(chatid, 'Центр киберспорта:', bookingOptions)
+        }
+        if (text === 'дурак') {
+            return bot.sendMessage(chatid, 'Это вот было обидно')
+        }
+        if (text === 'ты плохой') {
+            return bot.sendMessage(chatid, 'Поняла, исправлюсь')
+        }
+        if (text === 'ты бот') {
+            return bot.sendMessage(chatid, 'Ну, можно и так сказать')
+        }
+        return bot.sendMessage(chatid, `Введите '/start' чтобы начать`)
+    })
+
+    bot.on('callback_query', msg => {
+        const data = msg.data;
+        const chatid = msg.message.chat.id;
+        if (data === chats[chatid]) {
+            return bot.sendMessage(chatid,`Выберите центр: ${chats[chatid]}`, placeOptions)
+        }
+        console.log(msg)
+    })
+}
+start()
